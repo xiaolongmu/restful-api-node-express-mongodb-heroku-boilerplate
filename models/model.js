7ced9f26be107de9bb94240741616ef6a6adbbfd
sslover <samjslover@gmail.com>
2015-02-18 13:36:21 -0500

    updating docs

--- models/model.js
+++ models/model.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// See http://mongoosejs.com/docs/schematypes.html
+
 var personSchema = new Schema({
 	name: String,
 	locationGeo : { type: [Number], index: { type: '2dsphere', sparse: true } },
